af6a5ec083d1f0b50280cd43dce9bb0175a2959d
Nate <natejoe@gmail.com>
2016-04-27 12:09:21 -0400

    Add audio file and setup buttons to play sound when pressed.

--- Retro-Calculator/ViewController.swift
+++ Retro-Calculator/ViewController.swift
@@ -7,19 +7,91 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Equals = "="
+        case Empty = "Empty"
+    }
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    //variable for the button sound
+    var btnSound: AVAudioPlayer!
+    
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var currentOperation: Operation = Operation.Empty
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundURL = NSURL(fileURLWithPath: path!)
+        
+        do{
+            try btnSound = AVAudioPlayer(contentsOfURL: soundURL)
+            btnSound.prepareToPlay()
+        }
+        catch let err as NSError{
+            print(err.debugDescription)
+        }
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Function For number press
+    @IBAction func numberPressed(btn: UIButton!){
+        playSound()
+        runningNumber += "\(btn.tag)"
+        outputLabel.text = runningNumber
     }
-
-
+    
+    //Function when Divide button is pressed
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(Operation.Divide)
+    }
+    
+    //Function when Multiply is pressed
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(Operation.Multiply)
+    }
+    
+    //Funciton when Subtract is pressed
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(Operation.Subtract)
+    }
+    
+    //Function when Add is pressed
+    @IBAction func onAddPressed(sender: AnyObject) {
+        processOperation(Operation.Add)
+    }
+    
+    //Function when Equal is pressed
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(Operation.Equals)
+    }
+    
+    func processOperation(op: Operation) {
+        playSound()
+    }
+    
+    
+    func playSound() {
+        if btnSound.playing {
+            btnSound.stop()
+        }
+        
+        btnSound.play()
+    }
 }
 
